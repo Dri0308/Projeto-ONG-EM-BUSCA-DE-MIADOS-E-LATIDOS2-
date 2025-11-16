c79f370d340a5d539875048dd7e0596cd38c938e
Dri0308 <adriana.rn030812@gmail.com>
2025-11-16 00:34:35 -0300

    Adiciona templates de seção para navegação

--- index.js
+++ index.js
@@ -167,3 +167,24 @@ const exibirParceiros = () => {
 exibirParceiros();
 });
 
+const track =document.querySelector('gallery1', 'gallery2');
+const slides = Array.from(track.children);
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentIndex = 0;
+
+function updateCarousel() {
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateCarousel();
+});
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateCarousel();
+});
+
